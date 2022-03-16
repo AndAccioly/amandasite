beeb1a336b7404069e5b9fabd9d8eee537891d07
AndAccioly <andreacciolyl@hotmail.com>
2022-03-16 19:58:22 -0300

    Bugfix
    Another one

--- src/components/menu/MenuRecolhido.js
+++ src/components/menu/MenuRecolhido.js
@@ -10,19 +10,7 @@ import LinkMenu from './LinkMenu';
 function MenuRecolhido(props) {
     const classes = props.classes;
     return (
-        <PopupState variant="popover" popupId="menu-recolhido">
-          {(popupState) => (
-            <React.Fragment>
-              <MenuIcon {...bindTrigger(popupState)} class={classes.menuIcon}/>
-              <Menu {...bindMenu(popupState)}>
-                <MenuItem onClick={popupState.close}><LinkMenu classes={classes} texto={"Início"} url={"/home"}/></MenuItem>
-                <MenuItem onClick={popupState.close}><LinkMenu classes={classes} texto={"Sobre mim"} url={"/sobre-mim"}/></MenuItem>
-                <MenuItem onClick={popupState.close}><LinkMenu classes={classes} texto={"Artigos"} url={"/artigos"}/></MenuItem>
-                <MenuItem onClick={popupState.close}><LinkMenu classes={classes} texto={"Agendamento"} url={"/agendamento"}/></MenuItem>            
-              </Menu>
-            </React.Fragment>
-          )}
-        </PopupState>
+        <p>a</p>
       );
 }
 
