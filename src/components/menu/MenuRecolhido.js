a5f2e95e23d3f4cb00d96a444185aea6c2123dd1
AndAccioly <andreacciolyl@hotmail.com>
2022-03-16 20:35:35 -0300

    Bugfix

--- src/components/menu/MenuRecolhido.js
+++ src/components/menu/MenuRecolhido.js
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import LinkMenu from './LinkMenu';
+
 
 
 
