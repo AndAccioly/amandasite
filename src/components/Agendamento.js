7527b64c97039aaa192c533d1f75cbf26e88f21e
André <andreacciolyl@hotmail.com>
2022-09-07 14:12:19 -0300

    Email correction
    Email state corrections, now sending it works fine

--- src/components/Agendamento.js
+++ src/components/Agendamento.js
@@ -10,96 +10,99 @@ const BotaoAgendamento = styled(Button)(({ theme }) => ({
     color: 'black',
     backgroundColor: '#f5e689',
     '&:hover': {
-      backgroundColor: '#baac50',
+        backgroundColor: '#baac50',
     },
-  }));
+}));
 
-function Agendamento(props){
+function Agendamento(props) {
+    const OLA = "Olá, meu nome é "
+    const MOTIVOS = " e entro em contato pelo(s) seguinte(s) motivo(s): "
+    const ENTRE_EM_CONTATO = ". Entre em contato comigo por "
+    const OU = " ou "
+
     const DESTINATARIO = "andreacciolyl@gmail.com"
     const SERVICE_ID = "service_8x8pds6"
     const TEMPLATE_ID = "template_ft5kjie"
     const USER_ID = "wTUsBZq2cYLxssH9l"
-    const [email, setEmail] = useState({
-        de: '',
-        para: '',
-        nome: '',
-        mensagem: '',
-        replyTo: ''
-    });
+
     const [enderecoEmail, setEnderecoEmail] = useState("");
     const [nome, setNome] = useState("");
     const [telefone, setTelefone] = useState("");
     const [consulta, setConsulta] = useState("");
+    const [mensagem, setMensagem] = useState("")
     const classes = props.classes;
     const isContato = { inputProps: { 'aria-label': 'Checkbox contato' } };
 
     const enviarEmail = () => {
-        const mensagemMontada = "Olá, sou o(a) " + nome + " e entro em contato pelo(s) seguinte(s) motivo(s): " + consulta + ". Entre em contato comigo por " + telefone + " ou " + enderecoEmail
-        console.log(mensagemMontada)
-        setEmail({
+        const emailEnviar = {
             de: enderecoEmail,
             para: DESTINATARIO,
             nome: nome,
-            mensagem: mensagemMontada,
+            mensagem: mensagem,
             replyTo: ''
-        })
+        }
 
         send(
             SERVICE_ID,
             TEMPLATE_ID,
-            email,
+            emailEnviar,
             USER_ID
         ).then(
             (result) => {
-              console.log(result.text);
-              alert("SUCCESS!");
+                console.log(result.text);
+                alert("Email enviado com sucesso!");
             },
             (error) => {
-              console.log(error.text);
-              alert("FAILED...", error);
+                console.log(error.text);
+                alert("Erro ao enviar email. Tente novamente.", error);
             }
-          )
+        )
     }
 
     const onChangeEnderecoEmail = (e) => {
         setEnderecoEmail(e.target.value);
+        setMensagem(OLA + nome + MOTIVOS + consulta + ENTRE_EM_CONTATO + telefone + OU + e.target.value)
     }
     const onChangeNome = (e) => {
         setNome(e.target.value);
+        setMensagem(OLA + e.target.value + MOTIVOS + consulta + ENTRE_EM_CONTATO + telefone + OU + enderecoEmail)
     }
     const onChangeTelefone = (e) => {
         setTelefone(e.target.value);
+        setMensagem(OLA + nome + MOTIVOS + consulta + ENTRE_EM_CONTATO + e.target.value + OU + enderecoEmail)
     }
     const onChangeConsulta = (e) => {
         setConsulta(e.target.value);
+        setMensagem(OLA + nome + MOTIVOS + e.target.value + ENTRE_EM_CONTATO + telefone + OU + enderecoEmail)
     }
-    
+
     return (
         <div>
-            <div className = {classes.agendamentoForm}> 
-                <Box component="form" sx={{ '& > :not(style)': { m: 1}, alignItems: 'center', justifyContent: 'center', margin: '1%'}} noValidate  autoComplete="off">
+            <div className={classes.agendamentoForm}>
+                <Box component="form" sx={{ '& > :not(style)': { m: 1 }, alignItems: 'center', justifyContent: 'center', margin: '1%' }} noValidate autoComplete="off">
                     <p>Gostaria de agendar uma consulta?</p>
-                    <Box sx={{display: 'flex'} }>
-                        <div  className={classes.agendamentoFormItem} >
-                            <TextField id="agendamentoNome" label="Nome" variant="outlined" value={nome} onChange={onChangeNome}/></div>
-                        <div  className={classes.agendamentoFormItem} >
-                            <TextField id="agendamentoTelefone" label="Telefone" variant="outlined" value={telefone} onChange={onChangeTelefone}/></div>
-                        <div  className={classes.agendamentoFormItem} >
-                            <TextField id="agendamentoEmail" label="Email" variant="outlined" value={enderecoEmail} onChange={onChangeEnderecoEmail}/> </div>
+                    <Box sx={{ display: 'flex' }}>
+                        <div className={classes.agendamentoFormItem} >
+                            <TextField id="agendamentoNome" label="Nome" variant="outlined" value={nome} onChange={onChangeNome} /></div>
+                        <div className={classes.agendamentoFormItem} >
+                            <TextField id="agendamentoTelefone" label="Telefone" variant="outlined" value={telefone} onChange={onChangeTelefone} /></div>
+                        <div className={classes.agendamentoFormItem} >
+                            <TextField id="agendamentoEmail" label="Email" variant="outlined" value={enderecoEmail} onChange={onChangeEnderecoEmail} /> </div>
                     </Box>
-                    <Box sx={{display: 'block'}}>
-                        <div  className={classes.agendamentoFormText} ><TextField id="filled-multiline-static" label="Consulta" variant="outlined"
-                            multiline rows={4} fullWidth value={consulta} onChange={onChangeConsulta}/></div>
+                    <Box sx={{ display: 'block' }}>
+                        <div className={classes.agendamentoFormText} ><TextField id="filled-multiline-static" label="Consulta" variant="outlined"
+                            multiline rows={4} fullWidth value={consulta} onChange={onChangeConsulta} /></div>
                     </Box>
-                    <Box sx={{display: 'flex'} }>
+                    <Box sx={{ display: 'flex' }}>
                         <Checkbox {...isContato} defaultChecked sx={{
                             color: '#baac50', '&.Mui-checked': {
-                            color: '#baac50'}
-                        }}/>
+                                color: '#baac50'
+                            }
+                        }} />
                         <p>Autorizo que entrem em contato comigo através de Whatsapp, email ou telefone</p>
                     </Box>
                     <BotaoAgendamento onClick={enviarEmail}>Solicitar agendamento</BotaoAgendamento>
-                </Box>    
+                </Box>
             </div>
             <Box>
                 <p>Você pode agendar uma consulta também pelo telefone xxxx-xxxx ou email yyy@yyyy.com</p>
